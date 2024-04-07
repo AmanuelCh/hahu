0aa799a532f294f1b70123b5c7cefe48535a9491
AmanuelCh <amanuelchaka2@gmail.com>
2024-04-07 22:57:20 +0300

    feat: function support

--- index.js
+++ index.js
@@ -135,3 +135,26 @@ topScope.print = (value) => {
 function run(program) {
   return evaluate(parse(program), Object.create(topScope));
 }
+// add "function" syntax
+specialForms.fun = (args, scope) => {
+  if (!args.length) {
+    throw new SyntaxError('Functions need a body');
+  }
+  let body = args[args.length - 1];
+  let params = args.slice(0, args.length - 1).map((expr) => {
+    if (expr.type != 'word') {
+      throw new SyntaxError('Parameter names must be words');
+    }
+    return expr.name;
+  });
+  return function () {
+    if (arguments.length != params.length) {
+      throw new TypeError('Wrong number of arguments');
+    }
+    let localScope = Object.create(scope);
+    for (let i = 0; i < arguments.length; i++) {
+      localScope[params[i]] = arguments[i];
+    }
+    return evaluate(body, localScope);
+  };
+};
